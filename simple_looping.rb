c6c3bc407eca325712c225c3561ae107c0aa384d
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-30 13:19:24 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
   loop do
     counter = counter + 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    
+    if counter 
   
   
   # Code your solution here using the "loop" keyword to puts out the below phrase
