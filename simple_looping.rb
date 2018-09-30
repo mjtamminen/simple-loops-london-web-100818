32f2bb69e8ac333d7ec882a4d82ccc39db87dc58
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-30 13:27:14 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,7 +24,7 @@ end
 
 def times_iterator(number_of_times)
   #{number_of_times}.times do
-    puts = "Welcome to Flatiron School's Web Development Course!"
+    puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
 
