5df20846038f1e43ec9555418e93845ef49a6c5a
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-30 13:21:29 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
   loop do
     counter = counter + 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    if counter >= #{number_of_times}
+    if counter >= number_of_times
     break
   end
 end
